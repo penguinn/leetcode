a8c29918567a818ef780b178d62ae2ff74ab4a5f
penguinn <852198764@qq.com>
2021-01-09 13:32:29 +0800

    55-2 59-2

---
+++ interview/剑指offer/56-2.数组中数字出现的次数2/index_test.go
@@ -0,0 +1,27 @@
+package index
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{nums: []int{3, 4, 3, 3}},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
